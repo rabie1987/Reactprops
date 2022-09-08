f7b2c3008e42908709430c1d2805988bd2c42638
rabie tabbessi <rabietabbessi@gmail.com>
2022-09-08 18:20:41 +0100

    props

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Profile from "./Profile/Profile";
+import userImg from "./images/user.jpg";
+import "./styles.css";
+export default function App() {
+  function handleName(name) {
+    alert(`Hello my name is ${name}`);
+  }
+
+  return (
+    <div className="App">
+      <Profile
+        fullName="Rabie Tabbessi"
+        bio="Hello , my name is Rabie Tabbessi and i like web development"
+        profession="Full-Stack js Developer"
+        handleName={handleName}
+      >
+        <img
+          style={{ borderRadius: "100%", width: "200px", height: "200px" }}
+          src={userImg}
+          alt="avatar"
+        />
+      </Profile>
+    </div>
+  );
+}
